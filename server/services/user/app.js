1ea90e3fd819820faff63bfffb7ba138beabd876
RizkyMaulita <rizky.maulita@gmail.com>
2023-09-17 22:11:11 +0700

    remove lecture

--- server/services/user/app.js
+++ server/services/user/app.js
@@ -6,7 +6,6 @@ const PORT = Number(process.env.PORT) || 4001;
 app.get("/", (_, res) => {
   res.status(200).json({
     message: `Welcome to User Service with DB_URL ${process.env.DATABASE_URL}!`,
-    title: "Hacktiv8 - Phase 3 - Intro Docker",
     os: os.platform(),
   });
 });
